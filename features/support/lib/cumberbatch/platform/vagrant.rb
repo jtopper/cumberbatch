bcb9756ea4206781eabed9ebd21be16e67f9d360
Jon Topper <jon@scalefactory.com>
2012-04-27 00:39:17 +0100

    Make Vagrant calls verbose.  Fix race condition.
    Changed the way the Cumberbatch VM object accesses the Vagrant one.
    Previously, if the vagrant box was downloaded during the first use of
    cumberbatch, calls to the VM object didn't work as expected.

--- features/support/lib/cumberbatch/platform/vagrant.rb
+++ features/support/lib/cumberbatch/platform/vagrant.rb
@@ -7,7 +7,10 @@ module Cumberbatch
         class Vagrant < Base
 
             def initialize
-                @vagrant_env ||= ::Vagrant::Environment.new
+
+                @vagrant_env ||= ::Vagrant::Environment.new( 
+                    :ui_class => ::Vagrant::UI::Basic 
+                )
                 super()
             end
 
@@ -15,12 +18,7 @@ module Cumberbatch
 
             def create_vm_object_by_name( name )
 
-                vm = @vagrant_env.vms[ name.to_sym ]
-                if vm.nil?
-                    raise VMNotFoundError
-                end
-
-                return VagrantVM.new( @vagrant_env, vm )
+                return VagrantVM.new( @vagrant_env, name )
 
             end
 
@@ -28,13 +26,28 @@ module Cumberbatch
 
         class VagrantVM < VMBase
 
-            def initialize( vagrant_env, vagrant_vm )
-                @vagrant_env = vagrant_env
-                @vagrant_vm  = vagrant_vm
+            private
+
+            def vagrant_vm
+                return @vagrant_env.vms[ @vagrant_vm_name.to_sym ]
+            end
+
+            public
+
+            def initialize( vagrant_env, vagrant_vm_name )
+
+                @vagrant_env     = vagrant_env
+                @vagrant_vm_name = vagrant_vm_name
+
+                vm = @vagrant_env.vms[ @vagrant_vm_name.to_sym ]
+                if vm.nil?
+                    raise VMNotFoundError
+                end
+
             end
 
             def execute( command, &block )
-                @vagrant_vm.channel.execute(
+                vagrant_vm.channel.execute(
                     command,
                     opts = { :error_check => false },
                     &block
@@ -42,7 +55,7 @@ module Cumberbatch
             end
 
             def sudo( command, &block )
-                @vagrant_vm.channel.sudo(
+                vagrant_vm.channel.sudo(
                     command,
                     opts = { :error_check => false },
                     &block
@@ -50,23 +63,23 @@ module Cumberbatch
             end
 
             def upload( from, to )
-                @vagrant_vm.channel.upload( from, to )
+                vagrant_vm.channel.upload( from, to )
             end
 
             def start
-                @vagrant_env.cli('up', @vagrant_vm.name)
+                @vagrant_env.cli('up', vagrant_vm.name)
             end
             
             def stop
-                @vagrant_env.cli('halt', @vagrant_vm.name)
+                @vagrant_env.cli('halt', vagrant_vm.name)
             end
 
             def snapshot
-                @vagrant_env.cli('sandbox', 'on', @vagrant_vm.name)
+                @vagrant_env.cli('sandbox', 'on', vagrant_vm.name)
             end
 
             def rollback
-                @vagrant_env.cli('sandbox', 'rollback', @vagrant_vm.name)
+                @vagrant_env.cli('sandbox', 'rollback', vagrant_vm.name)
             end
 
             def guest
@@ -75,7 +88,7 @@ module Cumberbatch
                     return @guest
                 end
 
-                guest_klass = Cumberbatch.guests[ @vagrant_vm.config.vm.guest ]
+                guest_klass = Cumberbatch.guests[ vagrant_vm.config.vm.guest ]
                 @guest = guest_klass.new( self )
 
             end
@@ -92,7 +105,7 @@ module Cumberbatch
                 #  world of race conditions.
                 sleep 3
 
-                freeman_port_conf = @vagrant_vm.config.vm.forwarded_ports.find { 
+                freeman_port_conf = vagrant_vm.config.vm.forwarded_ports.find { 
                     |x| x[:name] == 'freeman' 
                 }
 
@@ -104,7 +117,7 @@ module Cumberbatch
             end
 
             def name
-                @vagrant_vm.name
+                vagrant_vm.name
             end
 
         end
